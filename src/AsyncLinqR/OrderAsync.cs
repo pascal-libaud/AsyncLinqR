c70d62d8358a33d05cbf27f6eb5cac93190217ae
Pascal Libaud <pascal.libaud@viseo.com>
2024-04-08 21:31:07 +0200

    OrderAsync

---
+++ src/AsyncLinqR/OrderAsync.cs
@@ -0,0 +1,24 @@
+﻿namespace AsyncLinqR;
+
+public static partial class AsyncLinq
+{
+    public static IAsyncOrderedEnumerable<T> OrderAsync<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default)
+    {
+        return new AsyncOrderedEnumerable<T, T>(source, x => x, null, true, cancellationToken);
+    }
+
+    public static IAsyncOrderedEnumerable<T> OrderAsync<T>(this IAsyncEnumerable<T> source, IComparer<T> comparer, CancellationToken cancellationToken = default)
+    {
+        return new AsyncOrderedEnumerable<T, T>(source, x => x, null, true, cancellationToken, comparer);
+    }
+
+    public static IAsyncOrderedEnumerable<T> OrderDescendingAsync<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default)
+    {
+        return new AsyncOrderedEnumerable<T, T>(source, x => x, null, false, cancellationToken);
+    }
+
+    public static IAsyncOrderedEnumerable<T> OrderDescendingAsync<T>(this IAsyncEnumerable<T> source, IComparer<T> comparer, CancellationToken cancellationToken = default)
+    {
+        return new AsyncOrderedEnumerable<T, T>(source, x => x, null, false, cancellationToken, comparer);
+    }
+}
