490120489fd44af1c5f56492299349fb5ef3c9dc
Pascal Libaud <pascal.libaud@outlook.fr>
2024-05-01 14:46:45 +0200

    New methods to do !

--- src/AsyncLinqR/_Todo.cs
+++ src/AsyncLinqR/_Todo.cs
@@ -1,4 +1,11 @@
 ﻿// TODO : Créer les méthodes ci-dessous
 
+// Insert
+
+// Average, Sum, Max, MaxBy, Min, MinBy
+// Contains
+// ElementAt, ElementAtOrDefault
+// GroupJoin
+
 // Nouveautés .Net 9
 // Enumerable.CountBy, AggregateBy
